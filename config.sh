a9b2d359cf521fbb06373cea118aae57300958c5
Dushyant <devops@bydushyant.com>
2022-05-16 22:29:41 +0530

    Format config file

--- config.sh
+++ config.sh
@@ -20,7 +20,7 @@ DOTFILES_CONF="$HOME/.dotfiles/configs"
 #     if [ "$1"  = "--link" ]; then
 #         case $OS in
 #             "Linux")
-                
+
 #             ;;
 #             *)
 #                 # leave as is
@@ -31,135 +31,130 @@ DOTFILES_CONF="$HOME/.dotfiles/configs"
 #     if [ "$1"  = "--unlink" ]; then
 #         case $OS in
 #             "Linux")
-                
+
 #             ;;
 #             *)
 #                 # leave as is
 #                 return
 #             ;;
 #         esac
-#     fi 
+#     fi
 # }
 
-
-git_config ()
-{
+git_config() {
     # Link config
-    if [ "$1"  = "--link" ]; then
+    if [ "$1" = "--link" ]; then
         case $OS in
-            "Linux")
-                 GIT_CONF="$HOME/.gitconfig"
-                if [ -f "$GIT_CONF" ]; then
-                    sudo mv $GIT_CONF $HOME/.gitconfig.pre-config-run.$TIME
-                fi
-                ln -s $DOTFILES_CONF/.gitconfig $GIT_CONF
-                ls -l $DOTFILES_CONF/.gitconfig
+        "Linux")
+            GIT_CONF="$HOME/.gitconfig"
+            if [ -f "$GIT_CONF" ]; then
+                sudo mv $GIT_CONF $HOME/.gitconfig.pre-config-run.$TIME
+            fi
+            ln -s $DOTFILES_CONF/.gitconfig $GIT_CONF
+            ls -l $DOTFILES_CONF/.gitconfig
             ;;
-            *)
-                # leave as is
-                return
+        *)
+            # leave as is
+            return
             ;;
         esac
     fi
     # Unlink config
-    if [ "$1"  = "--unlink" ]; then
+    if [ "$1" = "--unlink" ]; then
         case $OS in
-            "Linux")
-                GIT_CONF="$HOME/.gitconfig"
-                unlink  $GIT_CONF       
+        "Linux")
+            GIT_CONF="$HOME/.gitconfig"
+            unlink $GIT_CONF
             ;;
-            *)
-                # leave as is
-                return
+        *)
+            # leave as is
+            return
             ;;
         esac
     fi
 }
-zsh_config ()
-{
-    if [ "$1"  = "--link" ]; then
+zsh_config() {
+    if [ "$1" = "--link" ]; then
         case $OS in
-            "Linux")
-                 ZSH_CONF="$HOME/.zshrc"
-                if [ -f "$ZSH_CONF" ]; then
-                    sudo mv $ZSH_CONF $HOME/.zshrc.pre-config-run.$TIME
-                fi
-                ln -s $DOTFILES_CONF/.zshrc $ZSH_CONF
-                ls -l $DOTFILES_CONF/.zshrc
+        "Linux")
+            ZSH_CONF="$HOME/.zshrc"
+            if [ -f "$ZSH_CONF" ]; then
+                sudo mv $ZSH_CONF $HOME/.zshrc.pre-config-run.$TIME
+            fi
+            ln -s $DOTFILES_CONF/.zshrc $ZSH_CONF
+            ls -l $DOTFILES_CONF/.zshrc
             ;;
-            *)
-                # leave as is
-                return
+        *)
+            # leave as is
+            return
             ;;
         esac
     fi
-    if [ "$1"  = "--unlink" ]; then
+    if [ "$1" = "--unlink" ]; then
         case $OS in
-            "Linux")
-                ZSH_CONF="$HOME/.zshrc"
-                unlink  $ZSH_CONF  
+        "Linux")
+            ZSH_CONF="$HOME/.zshrc"
+            unlink $ZSH_CONF
             ;;
-            *)
-                # leave as is
-                return
-            ;;
-        esac
-    fi 
-}
-
-omz_config ()
-{
-    if [ "$1"  = "--link" ]; then
-        case $OS in
-            "Linux")
-                OMZ_CUSTOM="$HOME/.oh-my-zsh/custom"
-                OMZ_CONF_ALIASES="$OMZ_CUSTOM/alias.zsh"
-                OMZ_CONF_FUNCTIONS="$OMZ_CUSTOM/functions.zsh"
-                P10K_CONF="$HOME/.p10k.zsh"
-
-                if [ -f "$OMZ_CONF_ALIASES" ]; then
-                    sudo mv $OMZ_CONF_ALIASES $OMZ_CUSTOM/alias.zsh.pre-config-run.$TIME
-                    sudo mv $OMZ_CONF_FUNCTIONS $OMZ_CUSTOM/functions.zsh.pre-config-run.$TIME
-                    sudo mv $P10K_CONF $HOME/.p10k.pre-config-run.$TIME
-                fi
-                
-                ln -s $DOTFILES_CONF/.oh-my-zsh/custom/alias.zsh $OMZ_CONF_ALIASES
-                ls -l $OMZ_CONF_ALIASES
-
-                ln -s $DOTFILES_CONF/.oh-my-zsh/custom/alias.zsh $OMZ_CONF_FUNCTIONS
-                ls -l $OMZ_CONF_FUNCTIONS
-
-                ln -s $DOTFILES_CONF/.p10k.zsh $HOME
-                ls -l $P10K_CONF
-            ;;
-            *)
-                # leave as is
-                return
+        *)
+            # leave as is
+            return
             ;;
         esac
     fi
-    if [ "$1"  = "--unlink" ]; then
-        case $OS in
-            "Linux")
-                OMZ_CUSTOM="$HOME/.oh-my-zsh/custom"
-                OMZ_CONF_ALIASES="$OMZ_CUSTOM/alias.zsh"
-                OMZ_CONF_FUNCTIONS="$OMZ_CUSTOM/functions.zsh"
-                P10K_CONF="$HOME/.p10k.zsh"
+}
 
-                unlink $OMZ_CONF_ALIASES
-                unlink $OMZ_CONF_FUNCTIONS 
-                unlink $P10K_CONF
+omz_config() {
+    if [ "$1" = "--link" ]; then
+        case $OS in
+        "Linux")
+            OMZ_CUSTOM="$HOME/.oh-my-zsh/custom"
+            OMZ_CONF_ALIASES="$OMZ_CUSTOM/alias.zsh"
+            OMZ_CONF_FUNCTIONS="$OMZ_CUSTOM/functions.zsh"
+            P10K_CONF="$HOME/.p10k.zsh"
+
+            if [ -f "$OMZ_CONF_ALIASES" ]; then
+                sudo mv $OMZ_CONF_ALIASES $OMZ_CUSTOM/alias.zsh.pre-config-run.$TIME
+                sudo mv $OMZ_CONF_FUNCTIONS $OMZ_CUSTOM/functions.zsh.pre-config-run.$TIME
+                sudo mv $P10K_CONF $HOME/.p10k.pre-config-run.$TIME
+            fi
+
+            ln -s $DOTFILES_CONF/.oh-my-zsh/custom/alias.zsh $OMZ_CONF_ALIASES
+            ls -l $OMZ_CONF_ALIASES
+
+            ln -s $DOTFILES_CONF/.oh-my-zsh/custom/alias.zsh $OMZ_CONF_FUNCTIONS
+            ls -l $OMZ_CONF_FUNCTIONS
+
+            ln -s $DOTFILES_CONF/.p10k.zsh $HOME
+            ls -l $P10K_CONF
             ;;
-            *)
-                # leave as is
-                return
+        *)
+            # leave as is
+            return
             ;;
         esac
-    fi     
+    fi
+    if [ "$1" = "--unlink" ]; then
+        case $OS in
+        "Linux")
+            OMZ_CUSTOM="$HOME/.oh-my-zsh/custom"
+            OMZ_CONF_ALIASES="$OMZ_CUSTOM/alias.zsh"
+            OMZ_CONF_FUNCTIONS="$OMZ_CUSTOM/functions.zsh"
+            P10K_CONF="$HOME/.p10k.zsh"
+
+            unlink $OMZ_CONF_ALIASES
+            unlink $OMZ_CONF_FUNCTIONS
+            unlink $P10K_CONF
+            ;;
+        *)
+            # leave as is
+            return
+            ;;
+        esac
+    fi
 }
-# TODO Add any config option managed through this file. 
-list_help()
-{
+# TODO Add any config option managed through this file.
+list_help() {
     printf "$YELLOW_HL Config options.$NC\n"
     printf "$GREEN
     --link or -l (To create symbolic link between config file on .dotfiles project and link location)\n
@@ -174,53 +169,51 @@ list_help()
     $NC\n"
 }
 
-main()
-{
+main() {
     # Parse Action
     case $1 in
-        -l| --link) 
-            action="--link"
+    -l | --link)
+        action="--link"
         ;;
-        -u| --unlink)
-            action="--unlink"
+    -u | --unlink)
+        action="--unlink"
         ;;
-        --help| *)
-            list_help
-            exit 0
+    --help | *)
+        list_help
+        exit 0
         ;;
     esac
     action=$(echo $action | tr '[:upper:]' '[:lower:]')
 
     # Parse config arguments
     if [ $# -gt 1 ]; then
-        for args in "$@" 
-        do 
+        for args in "$@"; do
             args=$(echo $args | tr '[:upper:]' '[:lower:]') # Change input to lowercase
-            case $args in 
+            case $args in
             "git")
-                git_config $action 
-            ;;
+                git_config $action
+                ;;
             "zsh")
-                zsh_config $action 
-            ;;
+                zsh_config $action
+                ;;
             "omz")
                 omz_config $action
-            ;;
-            -l| --link| -u| --unlink| --help)
-                # do nothing it is parsed as action 
-            ;;   
+                ;;
+            -l | --link | -u | --unlink | --help)
+                # do nothing it is parsed as action
+                ;;
             *)
-                printf "$RED Configuration instruction for \"$args\" dose not exist.$NC" 
+                printf "$RED Configuration instruction for \"$args\" dose not exist.$NC"
                 list_help
-                exit 1   
-            ;;
+                exit 1
+                ;;
             esac
         done
 
-    else 
+    else
         # TODO add config function to cover in running all functions at once.
         git_config $action
-        zsh_config $action 
+        zsh_config $action
         omz_config $action
     fi
 }
