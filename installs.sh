133fdb8588524ae4ad521c129bf566135433a081
Dushyant <devops@bydushyant.com>
2022-05-24 22:37:47 +0530

    Code Formate

--- installs.sh
+++ installs.sh
@@ -22,11 +22,11 @@ draw_ascii() {
     # Create log file with system info
     LogFile="$HOME/.dotfiles/SYSTEM_INFO.log"
     if [ ! -f "$LogFile" ]; then
-    printf "OS Info\n\n$(cat /etc/os-release)\n\n\n" >>$LogFile
-    printf "CPU Info\n\n$(sudo lscpu)\n\n\n" >>$LogFile
-    printf "GPU Info\n\n$(sudo lshw -C display)\n\n\n" >>$LogFile
+      printf "OS Info\n\n$(cat /etc/os-release)\n\n\n" >>$LogFile
+      printf "CPU Info\n\n$(sudo lscpu)\n\n\n" >>$LogFile
+      printf "GPU Info\n\n$(sudo lshw -C display)\n\n\n" >>$LogFile
     fi
-  ;;
+    ;;
   "Darwin")
     printf "\n$GREEN
                             ____   _____ 
@@ -44,118 +44,114 @@ draw_ascii() {
 
 app_installs() {
 
-
-    for app in $(echo $1 | sed "s/;/ /g")
-    do
-      # Install Instruction for bitwarden 
-      if [ $app = "Bitwarden" ]; then
-        install_bitwarden
-      fi
-      if [ $app = "Authy" ]; then
-        install_authy
-      fi
-      # echo $app
-    done
-    # case $1 in
-    # Bitwarden)
-    #   printf "$WHITE$BLUE_HL Installing Bitwarden desktop client$NC\n"
-    #   desktop-file-validate $HOME/.dotfiles/linux/appimages/Bitwarden.desktop
-    #   desktop-file-install --dir=$HOME/.local/share/applications $HOME/.dotfiles/linux/appimages/Bitwarden.desktop
-    #   update-desktop-database $HOME/.local/share/applications
-    #   printf "$GREEN Bitwarden desktop client appimage is Installed$NC\n"
-    # ;;
-    # Authy)
-    #   printf "$WHITE$BLUE_HL Installing Authy 2FA TOTP generator$NC\n"
-    #   printf "$RED Installing Authy requires to install snap package manager to be installed$NC\n"
-    #   sudo apt install snapd
-    #   sudo snap install authy
-    #   printf "$GREEN authy snap package is installed Installed$NC\n"
-    # ;;
-    # Neofetch)
-    #   printf "$WHITE$BLUE_HL Installing Neofetch CLI utility$NC\n"
-    #   sudo apt install neofetch
-    # ;;
-    # Exa)
-    #   printf "$WHITE$BLUE_HL Installing exa cli colorized ls replacement tool$NC\n"
-    #   sudo apt install exa
-    # ;;
-    # VScode)
-    #   printf "$WHITE$BLUE_HL Installing Visual Studio Code$NC\n"
-    #   sudo dpkg --install $HOME/.dotfiles/linux/dpkg/vscode.deb
-    #   ;;
-    # Flameshot)
-    #   printf "$WHITE$BLUE_HL Installing Flameshot screenshot utility$NC\n"
-    #   sudo apt install flameshot
-    #   ;;
-    # CopyQ)
-    #   printf "$WHITE$BLUE_HL Installing CopyQ clipboard history utility$NC\n"
-    #   sudo add-apt-repository ppa:noobslab/indicators
-    #   sudo apt update
-    #   sudo apt install copyq
-    #   ;;
-    # *)
-    #   printf $1 
-    #   printf "$WHITE$BLUE_HL Option may not be configured.$NC\n"
-    #   # Do nothing here
-    #   ;;
-    # esac
+  for app in $(echo $1 | sed "s/;/ /g"); do
+    # Install bitwarden
+    if [ $app = "Bitwarden" ]; then
+      install_bitwarden
+    fi
+    # Install Authy
+    if [ $app = "Authy" ]; then
+      install_authy
+    fi
+    # echo $app
+  done
+  # case $1 in
+  # Bitwarden)
+  #   printf "$WHITE$BLUE_HL Installing Bitwarden desktop client$NC\n"
+  #   desktop-file-validate $HOME/.dotfiles/linux/appimages/Bitwarden.desktop
+  #   desktop-file-install --dir=$HOME/.local/share/applications $HOME/.dotfiles/linux/appimages/Bitwarden.desktop
+  #   update-desktop-database $HOME/.local/share/applications
+  #   printf "$GREEN Bitwarden desktop client appimage is Installed$NC\n"
+  # ;;
+  # Authy)
+  #   printf "$WHITE$BLUE_HL Installing Authy 2FA TOTP generator$NC\n"
+  #   printf "$RED Installing Authy requires to install snap package manager to be installed$NC\n"
+  #   sudo apt install snapd
+  #   sudo snap install authy
+  #   printf "$GREEN authy snap package is installed Installed$NC\n"
+  # ;;
+  # Neofetch)
+  #   printf "$WHITE$BLUE_HL Installing Neofetch CLI utility$NC\n"
+  #   sudo apt install neofetch
+  # ;;
+  # Exa)
+  #   printf "$WHITE$BLUE_HL Installing exa cli colorized ls replacement tool$NC\n"
+  #   sudo apt install exa
+  # ;;
+  # VScode)
+  #   printf "$WHITE$BLUE_HL Installing Visual Studio Code$NC\n"
+  #   sudo dpkg --install $HOME/.dotfiles/linux/dpkg/vscode.deb
+  #   ;;
+  # Flameshot)
+  #   printf "$WHITE$BLUE_HL Installing Flameshot screenshot utility$NC\n"
+  #   sudo apt install flameshot
+  #   ;;
+  # CopyQ)
+  #   printf "$WHITE$BLUE_HL Installing CopyQ clipboard history utility$NC\n"
+  #   sudo add-apt-repository ppa:noobslab/indicators
+  #   sudo apt update
+  #   sudo apt install copyq
+  #   ;;
+  # *)
+  #   printf $1
+  #   printf "$WHITE$BLUE_HL Option may not be configured.$NC\n"
+  #   # Do nothing here
+  #   ;;
+  # esac
 }
 
-install_bitwarden()
-{
-   case $OS in
-            "Linux")
-              case $DIST in
-              Ububtu | Pop!_OS)
-                printf "$WHITE$BLUE_HL Installing Bitwarden desktop client$NC\n"
-                desktop-file-validate $HOME/.dotfiles/linux/appimages/Bitwarden.desktop
-                desktop-file-install --dir=$HOME/.local/share/applications $HOME/.dotfiles/linux/appimages/Bitwarden.desktop
-                update-desktop-database $HOME/.local/share/applications
-                printf "$GREEN Bitwarden desktop client appimage is Installed$NC\n"
-              ;;
-              esac
-            ;;
-            "Darwin")
-                brew install bitwarden
-            ;;
-            *)
-                # leave as is
-                return
-            ;;
-        esac
-}
-
-install_authy()
-{
+install_bitwarden() {
   case $OS in
-            "Linux")
-              case $DIST in
-              Ububtu | Pop!_OS)
-                printf "$WHITE$BLUE_HL Installing Authy 2FA TOTP generator$NC\n"
-                printf "$RED Installing Authy requires to install snap package manager to be installed$NC\n"
-                sudo apt install snapd
-                sudo snap install authy
-                printf "$GREEN authy snap package is installed Installed$NC\n"
-              ;;
-              *)
-                printf "Script is not supporting $DIST Linux distribution yet."
-              ;;
-              esac
-            ;;
-            "Darwin")
-                printf "$WHITE$BLUE_HL Installing Authy 2FA TOTP generator$NC\n"
-                brew install --cask authy
-                printf "$GREEN authy snap package is installed Installed$NC\n"
-            ;;
-            *)
-                # leave as is
-                return
-            ;;
-        esac
+  "Linux")
+    case $DIST in
+    Ububtu | Pop!_OS)
+      printf "$WHITE$BLUE_HL Installing Bitwarden desktop client$NC\n"
+      desktop-file-validate $HOME/.dotfiles/linux/appimages/Bitwarden.desktop
+      desktop-file-install --dir=$HOME/.local/share/applications $HOME/.dotfiles/linux/appimages/Bitwarden.desktop
+      update-desktop-database $HOME/.local/share/applications
+      printf "$GREEN Bitwarden desktop client appimage is Installed$NC\n"
+      ;;
+    esac
+    ;;
+  "Darwin")
+    brew install bitwarden
+    ;;
+  *)
+    # leave as is
+    return
+    ;;
+  esac
 }
 
-main() 
-{
+install_authy() {
+  case $OS in
+  "Linux")
+    case $DIST in
+    Ububtu | Pop!_OS)
+      printf "$WHITE$BLUE_HL Installing Authy 2FA TOTP generator$NC\n"
+      printf "$RED Installing Authy requires to install snap package manager to be installed$NC\n"
+      sudo apt install snapd
+      sudo snap install authy
+      printf "$GREEN authy snap package is installed Installed$NC\n"
+      ;;
+    *)
+      printf "Script is not supporting $DIST Linux distribution yet."
+      ;;
+    esac
+    ;;
+  "Darwin")
+    printf "$WHITE$BLUE_HL Installing Authy 2FA TOTP generator$NC\n"
+    brew install --cask authy
+    printf "$GREEN authy snap package is installed Installed$NC\n"
+    ;;
+  *)
+    # leave as is
+    return
+    ;;
+  esac
+}
+
+main() {
   # Draw ascii art for specific system
   draw_ascii
 
